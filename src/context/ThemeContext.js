7bcac37dec529aef9cb35a25fe228930bebe63f7
Stefano Consonni <stefanoconsonni@ymail.com>
2022-08-27 10:23:47 +0200

    create ThemeContext using Context API

---
+++ src/context/ThemeContext.js
@@ -0,0 +1,7 @@
+import { createContext } from "react";
+
+const ThemeContext = createContext();
+
+export function ThemeProvider({ children }) {
+	return <ThemeContext.Provider value={{ color: "blue" }}>{children}</ThemeContext.Provider>;
+}
