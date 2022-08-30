65cecb04674d00ede84f2b6de373cf8e4348c23c
Stefano Consonni <stefanoconsonni@ymail.com>
2022-08-30 23:16:04 +0200

    add dark-mode styles to recipe cards

--- src/components/ThemeSelector.js
+++ src/components/ThemeSelector.js
@@ -1,4 +1,5 @@
 import { useTheme } from "../hooks/useTheme";
+import modeIcon from "../assets/mode-icon.svg";
 
 // styles
 import "./ThemeSelector.css";
@@ -6,10 +7,17 @@ import "./ThemeSelector.css";
 const themeColors = ["#58249c", "#249c6b", "#b70233"];
 
 export default function ThemeSelector() {
-	const { changeColor } = useTheme();
+	const { changeColor, changeMode, mode } = useTheme();
+
+	const toggleMode = () => {
+		changeMode(mode === "light" ? "dark" : "light");
+	};
 
 	return (
 		<div className="theme-selector">
+			<div className="mode-toggle">
+				<img onClick={toggleMode} style={{ filter: mode === "dark" ? "invert(100%)" : "invert(20%)" }} src={modeIcon} alt="light and dark mode icon" />
+			</div>
 			<div className="theme-buttons">
 				{themeColors.map((color) => (
 					<div key={color} onClick={() => changeColor(color)} style={{ background: color }} />
